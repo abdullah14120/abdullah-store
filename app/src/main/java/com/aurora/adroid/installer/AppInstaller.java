ed64e16378e1497c6ee3ed9f4d31454d9cbd1c04
abdullah14120 <abdullah.tamemii@gmail.com>
2026-05-06 14:16:54 +0300

    Update AppInstaller.java

--- app/src/main/java/com/aurora/adroid/installer/AppInstaller.java
+++ app/src/main/java/com/aurora/adroid/installer/AppInstaller.java
@@ -1,27 +1,17 @@
 /*
- * Aurora Droid
- * Copyright (C) 2019-20, Rahul Kumar Patel <whyorean@gmail.com>
- *
- * Aurora Droid is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Aurora Droid is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Aurora Droid.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Developed by: Abdullah Al-Tamimi
+ * Project: Custom Android Store (Aurora Based)
+ * Component: AppInstaller Factory - Defaulting to Zero-Restriction Mode
+ * * Original Copyright (C) 2019-20, Rahul Kumar Patel
  */
 
 package com.aurora.adroid.installer;
 
 import android.content.Context;
 
+import com.aurora.adroid.AuroraApplication;
 import com.aurora.adroid.Constants;
+import com.aurora.adroid.util.Log;
 import com.aurora.adroid.util.PrefUtil;
 
 public abstract class AppInstaller {
@@ -35,7 +25,11 @@ public abstract class AppInstaller {
                     INSTANCE = new AppInstaller() {
                         @Override
                         public InstallerBase getDefaultInstaller() {
+                            // جلب القيمة من الإعدادات
                             String prefValue = PrefUtil.getString(context, Constants.PREFERENCE_INSTALLATION_METHOD);
+                            
+                            Log.i("AppInstaller [" + AuroraApplication.DEVELOPER_SIGNATURE + "]: Method Selected -> " + prefValue);
+
                             switch (prefValue) {
                                 case "0":
                                     return new NativeInstaller(context);
@@ -43,10 +37,14 @@ public abstract class AppInstaller {
                                     return new RootInstaller(context);
                                 case "2":
                                     return new ServiceInstaller(context);
+                                case "3":
+                                    return new SessionInstaller(context);
                                 default:
+                                    // تعديل عبد الله التميمي: جعل SessionInstaller هو الخيار الافتراضي 
+                                    // لضمان عمل "بدون قيود" فور تحميل المتجر.
+                                    Log.i("Defaulting to SessionInstaller for Unrestricted access.");
                                     return new SessionInstaller(context);
                             }
-
                         }
                     };
                 }
