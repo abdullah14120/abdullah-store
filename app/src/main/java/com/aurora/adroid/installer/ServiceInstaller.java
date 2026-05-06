eff317893fa21203afc95da3218e839bf5eab366
abdullah14120 <abdullah.tamemii@gmail.com>
2026-05-06 14:22:47 +0300

    Update ServiceInstaller.java

--- app/src/main/java/com/aurora/adroid/installer/ServiceInstaller.java
+++ app/src/main/java/com/aurora/adroid/installer/ServiceInstaller.java
@@ -1,20 +1,8 @@
 /*
- * Aurora Droid
- * Copyright (C) 2019-20, Rahul Kumar Patel <whyorean@gmail.com>
- *
- * Aurora Droid is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Aurora Droid is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Aurora Droid.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Developed by: Abdullah Al-Tamimi
+ * Project: Custom Android Store (Aurora Based)
+ * Component: Service Installer - Privileged Extension Bridge
+ * * Original Copyright (C) 2019-20, Rahul Kumar Patel
  */
 
 package com.aurora.adroid.installer;
@@ -29,6 +17,7 @@ import android.os.RemoteException;
 
 import androidx.annotation.NonNull;
 
+import com.aurora.adroid.AuroraApplication;
 import com.aurora.adroid.BuildConfig;
 import com.aurora.adroid.Constants;
 import com.aurora.adroid.util.Log;
@@ -39,6 +28,7 @@ import java.io.File;
 
 public class ServiceInstaller extends InstallerBase {
 
+    // تفعيل خيار الاستبدال وتجاوز قيود الإصدار الأقدم (Downgrade) إذا كانت الخدمة تدعم ذلك
     private static final int ACTION_INSTALL_REPLACE_EXISTING = 2;
 
     public ServiceInstaller(Context context) {
@@ -55,26 +45,41 @@ public class ServiceInstaller extends InstallerBase {
         xInstall(packageName, Uri.fromFile(fileName));
     }
 
+    // دعم التثبيت غير المقيد عبر الخدمة المميزة
+    @Override
+    public void installApkUnrestricted(@NonNull String packageName, @NonNull String filePath) {
+        xInstall(packageName, Uri.fromFile(new File(filePath)));
+    }
 
     private void xInstall(@NonNull String packageName, @NonNull Uri fileUri) {
+        Log.i("ServiceInstaller [" + AuroraApplication.DEVELOPER_SIGNATURE + "]: Connecting to Privileged Extension for " + packageName);
+
         final ServiceConnection serviceConnection = new ServiceConnection() {
             public void onServiceConnected(ComponentName name, IBinder binder) {
                 IPrivilegedService service = IPrivilegedService.Stub.asInterface(binder);
                 IPrivilegedCallback callback = new IPrivilegedCallback.Stub() {
                     @Override
                     public void handleResult(String packageName, int returnCode) {
-
+                        Log.i("Privileged Install Result for " + packageName + ": " + returnCode);
+                        // يمكن هنا إرسال Broadcast بنتيجة التثبيت إذا لزم الأمر
                     }
                 };
                 try {
+                    // تنفيذ التثبيت بصلاحيات النظام عبر الخدمة
                     service.installPackage(
                             fileUri,
                             ACTION_INSTALL_REPLACE_EXISTING,
                             BuildConfig.APPLICATION_ID,
                             callback
                     );
+                    Log.i("Install command sent via Abdullah Al-Tamimi engine.");
                 } catch (RemoteException e) {
-                    Log.e("Connecting to privileged service failed");
+                    Log.e("RemoteException: Connecting to privileged service failed");
+                } finally {
+                    // فك الارتباط بالخدمة بعد إرسال الأمر لضمان عدم استهلاك الموارد
+                    try {
+                        context.getApplicationContext().unbindService(this);
+                    } catch (Exception ignored) {}
                 }
             }
 
@@ -85,6 +90,14 @@ public class ServiceInstaller extends InstallerBase {
 
         final Intent serviceIntent = new Intent(Constants.PRIVILEGED_EXTENSION_SERVICE_INTENT);
         serviceIntent.setPackage(Constants.PRIVILEGED_EXTENSION_PACKAGE_NAME);
-        context.getApplicationContext().bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        
+        try {
+            boolean isBound = context.getApplicationContext().bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+            if (!isBound) {
+                Log.e("Failed to bind to Privileged Service. Is the extension installed?");
+            }
+        } catch (Exception e) {
+            Log.e("Binding Error: " + e.getMessage());
+        }
     }
 }
